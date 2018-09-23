e735a341351f42ef2ec8e9b192cdf8af944fc4fa
Grey Wu <grey1991ss@gmail.com>
2018-09-23 20:05:20 +1000

    finish ass1 subset0

--- assignment/ass1/legit.pl
+++ assignment/ass1/legit.pl
@@ -7,6 +7,12 @@ sub Init {
     print "Initialized empty legit repository in .legit\n";
 }
 
+sub check_filename {
+  my $filename = shift @_;
+  die "legit.pl: error: invalid filename '$filename'\n"
+    if $filename !~ /^[a-zA-Z0-9]+[a-zA-Z0-9.\-_]*$/;
+}
+
 sub is_folder_empty {
     my $dirname = shift @_;
     opendir( my $dh, $dirname ) or die "Not a directory";
@@ -16,14 +22,16 @@ sub is_folder_empty {
 sub Add {
 
     # error if not init
-    die
-"legit.pl: error: no .legit directory containing legit repository exists\n"
+    die "legit.pl: error: no .legit directory containing legit repository exists\n"
       if !-d ".legit";
 
     # error if add a non-exist file
     foreach $file (@_) {
-        die "legit.pl: error: can not open '$file'\n" if !-f $file;
+      check_filename(@_);
+      die "legit.pl: error: can not open '$file'\n" if !-f $file;
     }
+
+
 
     # copy files to index
     foreach $file (@_) {
@@ -88,6 +96,26 @@ sub Commit {
     }
 }
 
+sub Log {
+  # error if not init
+  die "legit.pl: error: no .legit directory containing legit repository exists\n"
+    if !-d ".legit";
+
+  # error if more arguments
+  die "usage: legit.pl log\n"
+    if @_;
+
+  # error if not commit
+  die "legit.pl: error: your repository does not have any commits yet\n"
+    if is_folder_empty(".legit/commit");
+
+  open FILE, "<", ".legit/log.txt";
+  @lines = <FILE>;
+  print reverse @lines;
+  close FILE;
+}
+
+
 sub Show {
 
     # error if not init
@@ -96,16 +124,52 @@ sub Show {
 
     # error if not commit
     die "legit.pl: error: your repository does not have any commits yet\n"
-      if is_folder_empty(".legut/commit");
+      if is_folder_empty(".legit/commit");
 
     # error if no or more than one arguments
     die "usage: legit.pl show <commit>:<filename>\n"
-      if ( scalar @_ != 1 || $_[0] !~ /^.*:.*$/ );
+      if scalar @_ != 1;
 
+    # error if no ":"
+    die "legit.pl: error: invalid object $_[0]\n"
+      if $_[0] !~ /^.*:.*$/;
+
+    # get directory and filename
+    $show_dir = $_[0];
+    $show_dir =~ s/:.*$//;
+    $show_file = $_[0];
+    $show_file =~ s/^.*://;
+
+    # show files in index
+    if ($show_dir eq '') {
+      check_filename($show_file);
+      die "legit.pl: error: '$show_file' not found in index\n"
+        if !-f ".legit/index/$show_file";
+      open FILE, "<", ".legit/index/$show_file";
+      print $line while ($line = <FILE>);
+      close FILE;
+    }
+    # show files in commit
+    else {
+
+      # error no match commit
+      die "legit.pl: error: unknown commit '$show_dir'\n"
+        if !-d ".legit/commit/commit_$show_dir";
+
+      check_filename($show_file);
+
+      # error no match file in commit
+      die "legit.pl: error: '$show_file' not found in commit $show_dir\n"
+        if !-f ".legit/commit/commit_$show_dir/$show_file";
+      open FILE, "<", ".legit/commit/commit_$show_dir/$show_file";
+      print $line while ($line = <FILE>);
+      close FILE;
+    }
 }
 
 $arg = shift @ARGV;
 Init()        if $arg eq "init";
 Add(@ARGV)    if $arg eq "add";
 Commit(@ARGV) if $arg eq "commit";
+Log(@ARGV)    if $arg eq "log";
 Show(@ARGV)   if $arg eq "show";
