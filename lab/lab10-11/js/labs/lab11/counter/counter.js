f0248b98570fb64c97a7fc27157064e634391486
Grey Wu <grey1991ss@gmail.com>
2018-10-22 03:52:30 +1100

    finish lab11 lab12

--- lab/lab10-11/js/labs/lab11/counter/counter.js
+++
@@ -1,9 +0,0 @@
-(function() {
-   'use strict';
-   // write your code here
-   setInterval(getTime, 1000);
-   function getTime() {
-       let time = new Date().toLocaleTimeString();
-       document.getElementById('output').innerHTML = time;
-   }
-}());
