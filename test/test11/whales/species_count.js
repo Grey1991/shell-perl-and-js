293272ff0bf63f39c0f27fa7b332352b6ffa66e2
Grey Wu <grey1991ss@gmail.com>
2018-10-23 04:45:33 +1100

    finish whales

--- test/test11/whales/species_count.js
+++ test/test11/whales/species_count.js
@@ -1,7 +1,8 @@
 function species_count(target_species, whale_list) {
 
   // PUT YOUR CODE HERE
-  
+  return whale_list.filter(whale => whale.species === target_species).map(x=>x.how_many).reduce((a,b)=>a+b);
+
 }
 
 module.exports = species_count;
